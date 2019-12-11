3b47b30c49b29335f422e77e3d1bde9e051ee6f3
heikov123 <heiko.valli@khk.ee>
2019-12-12 00:50:30 +0200

    reviews

---
+++ server/src/user.js
@@ -0,0 +1,25 @@
+import { groupBy, map } from "ramda";
+import DataLoader from "dataloader";
+import query from "./db";
+
+async function findUsersByIds(ids) {
+  const sql = `
+    select * from hb.user
+    where hb.user.id = ANY($1);
+    `;
+  const params = [ids];
+  try {
+    const result = await query(sql, params);
+    const rowsById = groupBy(user => user.id, result.rows);
+    return map(id => {
+      const user = rowsById[id] ? rowsById[id][0] : null;
+      return user;
+    }, ids);
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+}
+export function findUsersByIdsLoader() {
+  return new DataLoader(findUsersByIds);
+}
