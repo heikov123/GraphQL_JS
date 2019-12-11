c3f9732688ccc4e392c88691ae640dbfa37d77cf
heikov123 <heiko.valli@khk.ee>
2019-12-12 00:34:28 +0200

    add imageURl

---
+++ server/src/db.js
@@ -0,0 +1,22 @@
+import { Pool } from "pg";
+import humps from "humps";
+
+const pool = new Pool({
+  host: "localhost",
+  database: "hackerbook"
+});
+
+async function query(sql, params) {
+  const client = await pool.connect();
+  try {
+    const result = await client.query(sql, params);
+    const rows = humps.camelizeKeys(result.rows);
+    return { ...result, rows };
+  } catch (err) {
+    console.log(err);
+  } finally {
+    client.release();
+  }
+}
+
+export default query;
