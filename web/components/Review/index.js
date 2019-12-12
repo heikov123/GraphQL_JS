d269ca821dfe77adbe2b7b711ed627adcbced4a4
heikov123 <heiko.valli@khk.ee>
2019-12-12 10:29:24 +0200

    files from beginning

---
+++ web/components/Review/index.js
@@ -0,0 +1,5 @@
+
+import Review from './Review';
+import ReviewInput from './ReviewInput';
+import RecentReviewSection from './RecentReviewSection';
+export { Review, RecentReviewSection, ReviewInput };
